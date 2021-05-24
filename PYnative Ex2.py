a0bb5998c575d29b625dad0b4919a6baf17ee0a1
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-05-24 17:56:02 +0100

    Add files via upload

---
+++ PYnative Ex2.py
@@ -0,0 +1,11 @@
+#PYnative Exercise 2:
+#Given a range of the first 10 numbers, Iterate from the start number to the end number, and In each iteration print the sum of the current number and previous number
+
+
+print("Printing current and previous number sum in a range(10):\n")
+previousnumber = 0
+for i in range(10):
+    currentnumber = i
+    sums = i + previousnumber
+    print("Current Number: " + (str(i)) + " -- Previous Number " + (str(previousnumber)) + " -- Sum: " + (str(sums)))
+    previousnumber = i
