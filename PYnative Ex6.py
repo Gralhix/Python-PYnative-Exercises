993afd5f4c9a943bff408560214557c701e81354
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-05-29 16:07:37 +0100

    Add files via upload

---
+++ PYnative Ex6.py
@@ -0,0 +1,22 @@
+# PYnative Exercise 6:
+# Given a list of numbers, Iterate it and print only those numbers which are divisible of 5
+
+
+# First will generate a random list of numbers (bit of code done for exercise 5 with minor alterations)
+
+import random
+
+randomlist = []
+for i in range (0,10):
+    n = random.randint(1,61)
+    randomlist.append(n)
+
+print("This is your randomly generated list of 10 numbers: " + str(randomlist))
+
+
+# Second part will iterate and print the ones that are divisible by 5
+
+for x in range (len(randomlist)):
+    if (randomlist[x]) % 5 == 0:
+        print(randomlist[x])
+ 
