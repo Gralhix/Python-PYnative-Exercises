f2d34824167151bf445c390172c78d43bdc57286
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-05-30 12:38:01 +0100

    Update PYnative Ex2.py

--- PYnative Ex2.py
+++ PYnative Ex2.py
@@ -1,8 +1,6 @@
 #PYnative Exercise 2:
-#Given a range of the first 10 numbers, Iterate from the start number to the end number, and In each iteration print the sum of the current number and previous number
+#Given a range of the first 10 numbers, Iterate from the start number to the end number, and in each iteration print the sum of the current number and previous number
 # https://pynative.com/python-basic-exercise-for-beginners/
-
-# Note: I'm very aware that this is a slightly more complex way of solving the exercise. Creating slightly more complicated solutions helps me learn.
 
 
 print("Printing current and previous number sum in a range(10):\n")
