be6d046eb503759e66260591199e45396550f2f9
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-05-30 12:35:38 +0100

    Update PYnative Ex6.py

--- PYnative Ex6.py
+++ PYnative Ex6.py
@@ -1,5 +1,8 @@
 # PYnative Exercise 6:
 # Given a list of numbers, Iterate it and print only those numbers which are divisible of 5
+# https://pynative.com/python-basic-exercise-for-beginners/
+
+# Note: I'm very aware that this is a slightly more complex way of solving the exercise. Creating slightly more complicated solutions helps me learn.
 
 
 # First will generate a random list of numbers (bit of code done for exercise 5 with minor alterations)
