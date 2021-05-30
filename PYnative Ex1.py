c4c89a6ee71c384a10a5c0767a21a0e81f371a4d
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-05-30 12:38:16 +0100

    Update PYnative Ex1.py

--- PYnative Ex1.py
+++ PYnative Ex1.py
@@ -1,5 +1,9 @@
 #PYnative Exercise 1
 #Given two integer numbers return their product. If the product is greater than 1000, then return their sum
+# https://pynative.com/python-basic-exercise-for-beginners/
+
+# Note: I'm very aware that this is a slightly more complex way of solving the exercise. Creating slightly more complicated solutions helps me learn.
+
 
 firstnumber = int(input("first number?\n"))
 secondnumber = int(input("second number?\n"))
