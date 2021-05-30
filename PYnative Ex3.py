d8008dfd47ffb7295eeec0322862dd614cf1609b
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-05-30 12:36:46 +0100

    Update PYnative Ex3.py

--- PYnative Ex3.py
+++ PYnative Ex3.py
@@ -1,5 +1,9 @@
 #PYnative Exercise 3:
 #Given a string, display only those characters which are present at an even index number.
+# https://pynative.com/python-basic-exercise-for-beginners/
+
+# Note: I'm very aware that this is a slightly more complex way of solving the exercise. Creating slightly more complicated solutions helps me learn.
+
 
 yourword = input("Please write a word. I will display the even characters.\n> ")
 i=0
