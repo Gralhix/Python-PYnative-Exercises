bbec5cc7cf97a2d5a9451269c63ba6ba6ef35aef
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-05-30 12:47:58 +0100

    Add files via upload

---
+++ PYnative Ex8.py
@@ -0,0 +1,12 @@
+# PYnative Exercise 8:
+# Print the following pattern
+# 1
+# 2 2
+# 3 3 3
+# 4 4 4 4
+# 5 5 5 5 5
+# https://pynative.com/python-basic-exercise-for-beginners/
+
+
+for i in range(1,6):
+    print ((str(i) + " " ) * i)
