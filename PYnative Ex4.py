63a45173a57b443e37f908280ed073665bdd1e28
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-05-30 12:36:17 +0100

    Update PYnative Ex4.py

--- PYnative Ex4.py
+++ PYnative Ex4.py
@@ -1,5 +1,9 @@
 # PYnative Exercise 4:
 # Given a string and an integer number n, remove characters from a string starting from zero up to n and return a new string
+# https://pynative.com/python-basic-exercise-for-beginners/
+
+# Note: I'm very aware that this is a slightly more complex way of solving the exercise. Creating slightly more complicated solutions helps me learn.
+
 
 print("You'll be asked to provide a word and then a number. The number will define how many characters to remove from your word starting from the beginning")
 
