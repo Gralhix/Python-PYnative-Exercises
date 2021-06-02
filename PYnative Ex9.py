1934e6d79896b2faf4ad62599978db1dcf137673
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-02 15:37:46 +0100

    Add files via upload

---
+++ PYnative Ex9.py
@@ -0,0 +1,18 @@
+# PYnative Exercise 9:
+# Reverse a given number and return true if it is the same as the original number
+
+
+# Will generate a random number between 100 and 999
+
+import random
+
+num = random.randint(100,1000)
+print(num)
+digit = (str(num))
+
+# Will check if the first and third numbers are the same
+
+if digit[0] == digit[2]:
+    print("The number is the same when reversed")
+else:
+    print("The number is  not the same when reversed")
