2629abf02ce7b30d7bb7bd9861c7153d1be7d6f8
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-04 13:03:13 +0100

    Add files via upload

---
+++ PYnative Ex13.py
@@ -0,0 +1,11 @@
+# PYnative Exercise 13:
+# Print multiplication table form 1 to 10
+
+# https://pynative.com/python-basic-exercise-for-beginners/
+
+
+for i in range (1,11):
+    for s in range (1,11):
+        print(i*s, end=" ")
+    print("\t")
+    
