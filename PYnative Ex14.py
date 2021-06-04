763c4b4124e5d5fc38f095216fe711014bd57ec4
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-04 13:09:35 +0100

    Add files via upload

---
+++ PYnative Ex14.py
@@ -0,0 +1,7 @@
+# PYnative Exercise 14:
+# Print downward Half-Pyramid Pattern with Star (asterisk)
+# https://pynative.com/python-basic-exercise-for-beginners/
+
+
+for i in range (5,0, -1):
+    print(" *"*i)
