671a47fa0924e85f31a2f4ec3de3d9d99ccf2237
Gralhix <74664918+Gralhix@users.noreply.github.com>
2021-06-26 18:00:42 +0100

    Add files via upload

---
+++ LoopsEx1.py
@@ -0,0 +1,8 @@
+# https://pynative.com/python-if-else-and-for-loop-exercise-with-solutions/
+# Python if else, for loop, and range() Exercise with Solutions
+# Exercise 1: Print First 10 natural numbers using while loop
+
+i = 0
+while i < 11:
+    print(i)
+    i += 1
